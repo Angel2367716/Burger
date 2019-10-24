108be09d81baff554913a950a99751e261768ec6
Marco Angel <angel2367716@gmail.com>
2019-10-23 23:26:04 -0700

    changed port on server file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgersController");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
